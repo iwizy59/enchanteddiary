1a605b9bb248125e93cb13b45095a2de635f36c2
Mathias <mathias.choquet1@gmail.com>
2024-03-13 14:19:38 +0100

    ajout calendar de base, interface uniquement ajout du bouton dans le main pour accéder au calendrier

---
+++ lib/calendar/calendar_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'calendar.dart'; // Assurez-vous que le chemin vers le widget du calendrier est correct
+
+class CalendarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page du Calendrier'),
+      ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(30),
+          child : CalendarWidget(), // Utilisez le widget du calendrier ici
+      ),
+      ),
+    );
+  }
+}
