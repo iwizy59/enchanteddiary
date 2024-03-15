b13e488f75cfcdaa81fbbcb5a822ba6f31ca495b
Mathias <mathias.choquet1@gmail.com>
2024-03-15 11:21:40 +0100

    modif pour set username config page

--- lib/pin/secret_question_config.dart
+++ lib/pin/secret_question_config.dart
@@ -9,10 +9,12 @@ class SecretQuestionConfigPage extends StatefulWidget {
 
 class _SecretQuestionConfigPageState extends State<SecretQuestionConfigPage> {
   final _answerController = TextEditingController();
+  final _answerUsernameController = TextEditingController();
 
   void _saveSecretAnswer() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString('secretAnswer', _answerController.text);
+    await prefs.setString('username', _answerUsernameController.text);
     await prefs.setBool('isFirstStart', false);
     Navigator.pushReplacement(
       context,
@@ -23,11 +25,13 @@ class _SecretQuestionConfigPageState extends State<SecretQuestionConfigPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Set Secret Question")),
+      appBar: AppBar(title: Text("Set Secret Question and Username")),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
+            Text("Enter your username"), // Exemple de question secrète
+            TextField(controller: _answerUsernameController),
             Text("What is your favorite color?"), // Exemple de question secrète
             TextField(controller: _answerController),
             ElevatedButton(onPressed: _saveSecretAnswer, child: Text("Save")),
