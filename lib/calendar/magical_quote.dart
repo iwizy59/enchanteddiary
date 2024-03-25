4b961eca8373a548c7a59251d2f80c7c1f3ca39a
Matt D <matt.desticourt@uphf.fr>
2024-03-25 11:57:01 +0100

    [FEATURE] Random magical quote on homepage

---
+++ lib/calendar/magical_quote.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class MagicalQuote extends StatelessWidget {
+  final Color darkBlue = Color(0xFF001244);
+  final Color lightBlue = Color(0xFF005086);
+  final Color skyBlue = Color(0xFF318fb5);
+  final Color lightGray = Color(0xFFb0cac7);
+  final Color lightYellow = Color(0xFFf7d6bf);
+
+  final List<String> magicalQuotes = [
+    "Believe in the magic within you.",
+    "Let the magic within you light up the world around you.",
+    "In a world of ordinary, dare to be enchanted.",
+    "Find the magic in everyday moments.",
+    "Let your imagination run wild and your dreams take flight.",
+    "Life's an adventure – sprinkle it with a little enchantment.",
+    "Embrace the whimsy of life and let the magic happen.",
+    "In a world of chaos, create your own enchanting moments.",
+    "Dance in the moonlight and let the stars guide your path.",
+    "Unleash your inner magic and let your spirit soar.",
+    "Open your heart to the enchantment all around you.",
+    "Explore, dream, discover – and never forget the magic within.",
+    "Life's too short to be ordinary – let's make it magical!"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    Random random = Random();
+    int index = random.nextInt(magicalQuotes.length);
+    String quote = magicalQuotes[index];
+
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: lightGray,
+      ),
+      child: Center(
+        child: Text(
+          quote,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'DancingScript',
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
